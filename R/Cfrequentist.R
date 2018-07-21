3ac50ce3473de4b31826e42587e5d9c705267e5e
Christina Sousa <christina.sousa85@gmail.com>
2018-07-20 18:12:04 -0700

    Working out my own version of frequentist test. The p-values seem reasonable but there are still bugs in the h2 side of the test.

--- R/Cfrequentist.R
+++ R/Cfrequentist.R
@@ -7,31 +7,35 @@ ASEoutlierCfrequentist<-function(filepath, output_columns = c("refCount","altCou
   for (i in 1:nrow(dat)){
     if ((dat[i,eh1]+dat[i,eh2])>8){
       #assume M is on eh1
-      r.obs<-(dat[i,eh1]/(dat[i,eh1]+dat[i,eh2]))
-      f.r.obs<-(-1-log2(1-r.obs))/sqrt(Vg)
+      r.obs.eh1<-dat[i,eh1]/(dat[i,eh1]+dat[i,eh2])
+      f.r.obs<-(-1-log2(1-r.obs.eh1))/sqrt(Vg)
       if (f.r.obs<=0){
-        output$p.val.eh1[i]<-2*pnorm(f.r.obs)
+        output$pval.eh1[i]<-2*pnorm(f.r.obs)
       }
       else{
         output$pval.eh1[i]<-2*pnorm(-f.r.obs)
       }
-      g.r.obs<-(-1-log2(r.obs))/sqrt(Vg)
+      #assume M is on eh2
+      r.obs.eh2<-(dat[i,eh2]/(dat[i,eh1]+dat[i,eh2]))
+      g.r.obs<-(-1-log2(r.obs.eh2))/sqrt(Vg)
       if (g.r.obs>=0){
-        output$p.val.eh2[i]<-2*pnorm(g.r.obs, lower.tail = FALSE)
+        output$pval.eh2[i]<-2*pnorm(g.r.obs, lower.tail = FALSE)
       }
       else{
         output$pval.eh2[i]<-2*pnorm(-g.r.obs, lower.tail = FALSE)
       }
-      output$pval.2sided[i]<-mean(c(output$pval.eh1,output$pval.eh2))
+      output$pval.2sided[i]<-mean(c(output$pval.eh1[i],output$pval.eh2[i]))
     }
     else{
-      output$p.val.eh1[i]<-NA
-      output$p.val.eh2[i]<-NA
-      output$p.val.2sided[i]<-NA
+      output$pval.eh1[i]<-NA
+      output$pval.eh2[i]<-NA
+      output$pval.2sided[i]<-NA
       }
   }
   #Carry out Benjamini-Hochberg procedure to get adjusted p-values
-  output$adj.pval<-p.adjust(output$p.val,method = "BH")
+  output$adj.pval.eh1<-p.adjust(output$pval.eh1,method = "BH")
+  output$adj.pval.eh2<-p.adjust(output$pval.eh2,method = "BH")
+  output$adj.pval.2sided<-p.adjust(output$pval.2sided,method = "BH")
   #output$sign<-(output$adj.pval<FDR)
   if (plot==TRUE){
     result<-output
@@ -40,7 +44,7 @@ ASEoutlierCfrequentist<-function(filepath, output_columns = c("refCount","altCou
     plot(result[,eh1], result[,eh2], log="xy", #yaxt = "n", xaxt = "n",
          main = "Reference Count vs. Alternative Count",
          xlab = "Reference Count", ylab = "Alternative Count",
-         col = ifelse(result$adj.pval<.05,'red','black'), pch = 19)
+         col = ifelse(result$adj.pval.2sided<.05,'red','black'), pch = 19)
     abline(a = 0, b = 1, col = "blue")
   }
   return(output)
