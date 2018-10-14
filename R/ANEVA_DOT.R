bbc63cb770828359ac94958f71c35a672d4b703b
Christina Sousa <christina.sousa85@gmail.com>
2018-10-14 10:00:14 -0400

    Integral blows up with this version of p-val calculation

--- R/ANEVA_DOT.R
+++ R/ANEVA_DOT.R
@@ -148,7 +148,7 @@ Binom_test_ctm_dbl<-function(X,N,p1,p2,log_BinCoeff,r0){
 
       tpl<-sum(Bnp[1:X])
       tpr<-sum(Bnp[(X+2):(length(Bnp))])
-      p.val[i]<-2*min(tpl,tpr,Bnp[X+1])
+      p.val[i]<-(2*min(tpl,tpr))+Bnp[X+1]
     }
   }
   return(p.val)
